3ed0f8a8b7f6c9213bfc4a644756b1dceec9947f
kasspulliam <kasspulliam@gmail.com>
2025-10-02 21:24:04 -0500

    Update contacts.py contacts

---
+++ pages/contacts.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Contacts", layout="wide")
+st.header("Contacts")
+st.write("If you need to contact us, you can reach us at our business email!")
+st.write("If you have any questions about anything that you cannot find answers too on our website,"
+         "don't hesitate to reach out!")
+st.write("Email: pixiepoppartiess@gmail.com")
